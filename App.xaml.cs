bda61c4d57bc39afef65db31684b9d09c71c9010
JayXie9499 <tsum950210@gmail.com>
2025-08-19 01:59:33 +0800

    fix: Solve class conflict with WindowsForms

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Windows;
-
-namespace Crosshair
+﻿namespace Crosshair
 {
-    public partial class App : Application { }
+    public partial class App : System.Windows.Application { }
 }
